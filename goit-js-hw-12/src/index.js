b15a14a78be96925e3ad9730039ea110c7d0a928
Roman Droga <romandroga@gmail.com>
2020-03-23 16:42:17 +0200

    new file:   .babelrc 	new file:   .browserslistrc 	new file:   .editorconfig 	new file:   .gitignore 	new file:   .prettierrc 	new file:   README.md 	new file:   build/images/bg-pattern.png 	new file:   build/index.html 	new file:   build/main.0.7274a03467937b00746c.css 	new file:   build/main.cbe33ba04c2e143a6f7e.js 	new file:   build/main.cbe33ba04c2e143a6f7e.js.map 	new file:   build/runtime.3c4283ca59e8321f4eda.js 	new file:   build/runtime.3c4283ca59e8321f4eda.js.map 	new file:   build/vendors.2.0574d6e491766bc12668.css 	new file:   build/vendors.a5c858915651d4762c70.js 	new file:   build/vendors.a5c858915651d4762c70.js.map 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   src/images/bg-pattern.png 	new file:   src/index.html 	new file:   src/index.js 	new file:   src/js/createMarkdown.js 	new file:   src/js/fetchCountries.js 	new file:   src/styles.css 	new file:   src/templates/card-template.hbs 	new file:   webpack/config.js 	new file:   webpack/configs/development.js 	new file:   webpack/configs/production.js 	new file:   webpack/configs/shared.js 	new file:   webpack/utils/paths.js

---
+++ goit-js-hw-12/src/index.js
@@ -0,0 +1,53 @@
+import './styles.css';
+import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+import fetchCountries from './js/fetchCountries';
+import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
+import createCard from './templates/card-template.hbs';
+
+const debounce = require('../node_modules/lodash.debounce');
+
+const refs = {
+  input: document.querySelector('#input'),
+  resultList: document.querySelector('.result-list'),
+};
+
+refs.input.addEventListener('input', debounce(searchCountries, 500));
+
+function searchCountries(e) {
+  resetMarkdown();
+  if (e.target.value !== '') {
+    fetchCountries(e.target.value)
+      .then(data => {
+        if (data.length > 10) {
+          createNotification();
+          return;
+        }
+        createMarkdown(data);
+      })
+      .catch(error => console.error(error));
+  }
+}
+
+function createMarkdown(data) {
+  if (data.length >= 2 && data.length <= 10) {
+    createCountryList(data);
+    return;
+  }
+  createCountryCard(...data);
+}
+
+function createCountryList(data) {
+  const markup = data.reduce((acc, elem) => acc + `<li>${elem.name}</li>`, '');
+  refs.resultList.insertAdjacentHTML('beforeend', markup);
+}
+function createCountryCard(data) {
+  refs.resultList.insertAdjacentHTML('beforeend', createCard(data));
+}
+function createNotification() {
+  PNotify.error({
+    text: 'Too many matches found.Please enter a more specific querry',
+  });
+}
+function resetMarkdown() {
+  refs.resultList.innerHTML = ' ';
+}
