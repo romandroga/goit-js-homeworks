61c73023b0c25787d4cee4626c494679a391e49f
Roman Droga <romandroga@gmail.com>
2020-03-26 21:39:41 +0200

    modified:   README.md 	new file:   build/images/bg-pattern.png 	new file:   build/index.html 	new file:   build/main.0.74f76eef32afff4a0654.css 	new file:   build/main.414b05d01e3979f89ddb.js 	new file:   build/main.414b05d01e3979f89ddb.js.map 	new file:   build/runtime.3c4283ca59e8321f4eda.js 	new file:   build/runtime.3c4283ca59e8321f4eda.js.map 	new file:   build/vendors.25a7b6543c6c7960e7f0.js 	new file:   build/vendors.25a7b6543c6c7960e7f0.js.map 	modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js 	modified:   src/templates/create-card-template.hbs

--- goit-js-hw-13/src/index.js
+++ goit-js-hw-13/src/index.js
@@ -1,8 +1,6 @@
 import './styles.css';
-import Masonry from '../node_modules/masonry-layout';
 import InfiniteScroll from '../node_modules/infinite-scroll';
 import cardTemplate from './templates/create-card-template.hbs';
-import * as basicLightbox from 'basiclightbox';
 
 const refs = {
   form: document.querySelector('#search-form'),
@@ -10,7 +8,7 @@ const refs = {
   baseUrl:
     'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=',
   key: '15734458-d3f6ccb7088e2e7a90b6df1f7',
-  proxy: "https://cors-anywhere.herokuapp.com/"
+  proxy: 'https://cors-anywhere.herokuapp.com/',
 };
 
 let searchQuerry;
@@ -19,20 +17,20 @@ const infScroll = new InfiniteScroll(refs.gallery, {
   responseType: 'text',
   history: false,
   path() {
-    return `${refs.proxy}${refs.baseUrl}${searchQuerry}&page=${this.pageIndex}&per_page=12&key=${refs.key}`;
+    return `${refs.baseUrl}${searchQuerry}&page=${this.pageIndex}&per_page=12&key=${refs.key}`;
   },
 });
 
 infScroll.on('load', response => {
   const cards = JSON.parse(response);
-  const markup = cards.hits.reduce((acc, elem) => acc + cardTemplate(elem));
-  const proxyElement = document.createElement('div');
-  proxyElement.innerHTML = markup;
-  const parsedItems = proxyElement.children;
-  infScroll.appendItems(parsedItems);
+  if (cards.total) {
+    const markup = cards.hits.reduce((acc, elem) => acc + cardTemplate(elem));
+    const proxyElement = document.createElement('div');
+    proxyElement.innerHTML = markup;
+    const parsedItems = proxyElement.children;
+    infScroll.appendItems(parsedItems);
+  }
 });
-
-refs.gallery.addEventListener('click', openModalWindow);
 
 refs.form.addEventListener('submit', e => {
   if (e.currentTarget.elements.query.value !== '') {
@@ -43,14 +41,3 @@ refs.form.addEventListener('submit', e => {
   }
   e.preventDefault();
 });
-
-function openModalWindow(e) {
-  if (e.target.tagName === 'IMG') {
-    basicLightbox
-      .create(`<img src="${e.target.dataset.source}" width="1280">`,)
-      .show();
-  }
-}
-
-
-// refs.gallery.addEventListener('click', (e)=> console.log(`${refs.proxy}${e.target.dataset.source}`));
